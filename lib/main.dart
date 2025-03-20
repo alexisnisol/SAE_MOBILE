91342f67e15396397e12613cf57285627984779b
Mouadzouadi <mouad.zouadi@etu.univ-orleans.fr>
2025-03-20 12:09:09 +0100

    home page avec bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sae_mobile/widgets/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Taste&Tell',
+      home : const Home(),
+      debugShowCheckedModeBanner: false,
+
+
     );
   }
 }
