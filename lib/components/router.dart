e0a5c5194dd6907a2f26bf8ddb129bb115a730d2
Mouadzouadi <mouad.zouadi@etu.univ-orleans.fr>
2025-03-24 17:09:58 +0100

    Configuration du router avec nav bar

--- lib/components/router.dart
+++ lib/components/router.dart
@@ -1,11 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sae_mobile/widgets/home.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
-  routes: <RouteBase> [
-    GoRoute(path: '/', builder: (context, state) => HomeScreen()),
-    GoRoute(path: '/register', builder: (context, state) => HomeScreen()),
-
+  routes: [
+    ShellRoute(
+      builder: (BuildContext context, GoRouterState state, Widget child) {
+        return HomeScreen(child: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const Center(child: Text('Accueil')),
+        ),
+        GoRoute(
+          path: '/favoris',
+          builder: (context, state) => const Center(child: Text('Favoris')),
+        ),
+        GoRoute(
+          path: '/carte',
+          builder: (context, state) => const Center(child: Text('Carte')),
+        ),
+        GoRoute(
+          path: '/avis',
+          builder: (context, state) => const Center(child: Text('Avis')),
+        ),
+        GoRoute(
+          path: '/profil',
+          builder: (context, state) => const Center(child: Text('Profil')),
+        ),
+      ],
+    ),
   ],
 );
--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,53 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  final Widget child;
+  const HomeScreen({super.key, required this.child});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-  final TextEditingController _searchController = TextEditingController();
-
-  static final List<Widget> pages = <Widget>[
-    Center(child: Text('Home')),
-    Center(child: Text('Favoris')),
-    Center(child: Text('Carte')),
-    Center(child: Text('Avis')),
-    Center(child: Text('Profil')),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+  int _getIndexFromRoute(BuildContext context) {
+    final currentLocation = GoRouter.of(context).state.uri.path;
+    switch (currentLocation) {
+      case '/':
+        return 0;
+      case '/favoris':
+        return 1;
+      case '/carte':
+        return 2;
+      case '/avis':
+        return 3;
+      case '/profil':
+        return 4;
+      default:
+        return 0;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final selectedIndex = _getIndexFromRoute(context);
     return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          controller: _searchController,
-          decoration: InputDecoration(
-            hintText: 'Rechercher...',
-            border: InputBorder.none,
-            prefixIcon: Icon(Icons.search),
-          ),
-        ),
-      ),
-      body: pages[_selectedIndex],
+      body: child,
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              context.go('/');
+              break;
+            case 1:
+              context.go('/favoris');
+              break;
+            case 2:
+              context.go('/carte');
+              break;
+            case 3:
+              context.go('/avis');
+              break;
+            case 4:
+              context.go('/profil');
+              break;
+          }
+        },
         selectedItemColor: Theme.of(context).primaryColor,
         unselectedItemColor: Colors.grey,
-        items: const <BottomNavigationBarItem>[
+        showUnselectedLabels: true,
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: 'Home',
+            label: 'Accueil',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.star),
