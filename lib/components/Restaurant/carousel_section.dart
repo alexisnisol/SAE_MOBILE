e703195d2c30baa90d02c296b8786ec8c8fca905
AlexyWCK <alex.wiciak@gmail.com>
2025-04-02 10:29:21 +0200

    Fix merge errors

--- lib/components/Restaurant/carousel_section.dart
+++ lib/components/Restaurant/carousel_section.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import '../../models/restaurant.dart';
+import 'package:geolocator/geolocator.dart';
+
+import 'card_restaurant.dart';
 
 class CarouselSection extends StatelessWidget {
+  final List<Restaurant> restaurants;
+  final Position? userPosition;
 
+  const CarouselSection({
+    super.key,
+    required this.restaurants,
+    this.userPosition,
+  });
 
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
       items: restaurants.map((restaurant) {
+        return RestaurantCard(
+          restaurant: restaurant,
+          userPosition: userPosition,
+        );
       }).toList(),
       options: CarouselOptions(
+        autoPlay: false,
+        autoPlayInterval: const Duration(seconds: 3),
+        autoPlayAnimationDuration: const Duration(milliseconds: 800),
         viewportFraction: 0.7,
         aspectRatio: 1.3,
         enableInfiniteScroll: true,
-        scrollDirection: Axis.horizontal,
       ),
     );
   }
