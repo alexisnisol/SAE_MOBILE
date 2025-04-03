40efbd1e4d5d60098b4535a57438c780db7d4b91
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2025-04-03 19:29:07 +0200

    Ajout import pour conflit ultérieur

--- lib/widgets/restaurant_detail_screen.dart
+++ lib/widgets/restaurant_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:sae_mobile/models/review.dart';
 import 'package:url_launcher/url_launcher.dart';
 
